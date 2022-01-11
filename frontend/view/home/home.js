4e03d69d39eaf69f182aa8e722f0dbd1b2b51852
Axel Fayet <axel.fayet97@gmail.com>
2022-01-11 17:26:07 +0100

    Mis en place titres

--- frontend/view/home/home.js
+++ frontend/view/home/home.js
@@ -4,52 +4,68 @@
 
 // CONNEXION A L'API
 
-// function fetchArticles() {
-//     fetch("http://localhost:4000/api/article/")
-//         .then(function (res) {
-//             if (res.ok) {
-//                 // get promise
-//                 var resultat = res.json();
-//                 // console.log("resultat" + resultat);
+function fetchArticles() {
+    fetch("http://localhost:4000/api/article/")
+        .then(function (res) {
+            if (res.ok) {
+                // get promise
+                return res.json();
+            }
+        })
+        .then(function (value) {
+            // console.log("API OK", value);
+            for (var i in value) {
+                document
+                    .querySelector(".container")
+                    .innerHTML +=
+        //             '"<div class="col-12 mt-5">
+        // <div class="card article">
+        //     <div class="card-header ">
+        //         <h5 class="card-title d-flex justify-content-between>`${ value[i].title + '<br /> '}`<span class="publication-date">{{ date de publication }}</span></h5>
+        //     </div>
+        //     <img src="{{ image }}" class="card-img-top">
+        //     <span class="fa-stack fa-2x addFavorite">
+        //         <i class="fas fa-star fa-stack-1x"></i>
+        //         <i class="far fa-star fa-stack-1x"></i>
+        //     </span>
+        //     <div class="card-body">
+        //         <p class="card-text">{{ Contenu }}</p>
+        //     </div>
+        // </div>
+        // </div>';
+                    value[i].title + '<br /> ';
+            }
+
+        })
+        .catch(function (err) {
+            // Une erreur est survenue
+            console.log('Une erreur est survenue.')
+        });
+};
+
+document.addEventListener("load", fetchArticles());
 
 
-//                 return resultat;
-//             }
-//         })
-//         .then(function (valueDeSortie) {
-//             console.log("API OK", valueDeSortie);
-//             return valueDeSortie;
-//         })
-//         .catch(function (err) {
-//             // Une erreur est survenue
-//             console.log('Une erreur est survenue.')
-//         });
-// };
-
-// document
-//     .addEventListener("load", fetchArticles());
+// var modifArticles = fetch("http://localhost:4000/api/article/")
+//     .then(function (res) {
+//         if (res.ok) {
+//             // get promise
+//             var resultat = res.json();
+//             // console.log("resultat" + resultat);
 
 
-var modifArticles = fetch("http://localhost:4000/api/article/")
-    .then(function (res) {
-        if (res.ok) {
-            // get promise
-            var resultat = res.json();
-            // console.log("resultat" + resultat);
+//         }
+//     })
+//     .then(function (valueDeSortie) {
+//         console.log("API OK", valueDeSortie);
+//         return valueDeSortie;
+//     })
+//     .catch(function (err) {
+//         // Une erreur est survenue
+//         console.log('Une erreur est survenue.')
+//     });
 
-            
-        }
-    })
-    .then(function (valueDeSortie) {
-        console.log("API OK", valueDeSortie);
-        return valueDeSortie;
-    })
-    .catch(function (err) {
-        // Une erreur est survenue
-        console.log('Une erreur est survenue.')
-    });
-
-console.log("valeur de sortie" + resultat);
+// console.log("valeur de sortie" + resultat);
 
 
 // for each articles in article : display ce modèle
@@ -58,26 +74,3 @@ console.log("valeur de sortie" + resultat);
 //     console.log("valeur" + value);
 // }
 
-
-
-//     for (let elements in fetchArticles()) {
-//         console.log(elements);
-//     };
-// document
-//     .querySelector(".container")
-//     .innerHTML +=
-//     `<div class="col-12 mt-5">
-//         <div class="card article">
-//             <div class="card-header ">
-//                 <h5 class="card-title d-flex justify-content-between><span class="publication-date">{{ date de publication }}</span></h5>
-//             </div>
-//             <img src="{{ image }}" class="card-img-top">
-//             <span class="fa-stack fa-2x addFavorite">
-//                 <i class="fas fa-star fa-stack-1x"></i>
-//                 <i class="far fa-star fa-stack-1x"></i>
-//             </span>
-//             <div class="card-body">
-//                 <p class="card-text">{{ Contenu }}</p>
-//             </div>
-//         </div>
-//         </div>`;
